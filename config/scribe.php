158814c05cdcbc745af9e1b817bf4923791b4027
delma60 <oladeleofficial@gmail.com>
2025-08-20 06:44:24 +0100

    another thing

--- config/scribe.php
+++ config/scribe.php
@@ -1,6 +1,5 @@
 <?php
 
-use Knuckles\Scribe\Config\AuthIn;
 use Knuckles\Scribe\Extracting\Strategies;
 use Knuckles\Scribe\Config\Defaults;
 use function Knuckles\Scribe\Config\{removeStrategies, configureStrategy};
@@ -102,7 +101,7 @@ return [
         'default' => "bearer",
 
         // Where is the auth value meant to be sent in a request?
-        'in' => AuthIn::BEARER->value,
+        'in' => 'bearer',
 
         // The name of the auth parameter (e.g. token, key, apiKey) or header (e.g. Authorization, Api-Key).
         'name' => 'Authorization',
