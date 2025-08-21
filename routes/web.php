d520f8ed5bf7eb46ecd6a3552091cc1335e5a596
delma60 <oladeleofficial@gmail.com>
2025-08-21 08:09:30 +0100

    add welcome

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 
+Route::get("/", function(){
+    return view("welcome");
+});
 
 Route::middleware("auth:sanctum")->group(function (){
     Route::prefix("service")->group(function() {
