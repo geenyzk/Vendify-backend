5f924d313255089c0e70eed8ce7299c5c1a70b1a
delma60 <oladeleofficial@gmail.com>
2025-08-24 18:17:06 +0100

    updater

--- config/cors.php
+++ config/cors.php
@@ -2,29 +2,13 @@
 
 return [
 
-<<<<<<< HEAD
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'docs/*'],
-
-    'allowed_methods' => ['*'],
-
-    //'allowed_origins' => ['http://localhost:8000', 'http://127.0.0.1:8000'],
-    'allowed_origins' => ['*'], //allow all origins for development, change for production.
-=======
     'paths' => ['api/*'],
-
     'allowed_methods' => ['*'],
-
     'allowed_origins' => ['*'],
->>>>>>> 5a8861e (Jush)
-
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => true,
 
 ];
